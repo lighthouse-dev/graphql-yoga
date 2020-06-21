afb8763735e8ffbe4839dd86a1a4117ac0da7aa8
lighthouse-dev <lighthouse2.dev@gmail.com>
2020-06-21 12:30:50 +0900

    :+1: add deleteMovie()

--- graphql/db.js
+++ graphql/db.js
@@ -1,18 +1,18 @@
 export const people = [
   {
-    id: '1',
+    id: 1,
     name: 'julee',
     age: '18',
     gender: 'male'
   },
   {
-    id: '2',
+    id: 2,
     name: 'hoge',
     age: '28',
     gender: 'male'
   },
   {
-    id: '3',
+    id: 3,
     name: 'hoge',
     name: 'fuga',
     age: '38',
@@ -22,48 +22,47 @@ export const people = [
 
 let movies = [
   {
-    id: '1',
+    id: 1,
     name: 'Avengers - The new one',
     score: 8
   },
   {
-    id: '2',
+    id: 2,
     name: 'The Godfather I',
     score: 9
   },
   {
-    id: '3',
+    id: 3,
     name: 'Logan',
     score: 7
   }
 ];
 
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
+  const filteredPeople = people.filter(person => person.id === id);
   return filteredPeople[0];
 };
 
-export const getMovies = movies;
+export const getMovies = () => movies;
 
 export const getMoviesById = id => {
-  const filteredMovie = movies.filter(movie => movie.id === String(id));
+  const filteredMovie = movies.filter(movie => movie.id === id);
   return filteredMovie[0];
 };
 
 export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id === String(id));
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
 
   if (movies.length > cleanedMovies.length) {
     movies = cleanedMovies;
     return true;
-  } else {
-    return false;
   }
+  return false;
 };
 
 export const addMovie = (name, score) => {
   const newMovie = {
-    id: `${movies.length + 1}`,
+    id: Number(`${movies.length + 1}`),
     name,
     score
   };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,22 @@
-import { people, getById, getMovies, getMoviesById, addMovie } from './db';
+import {
+  people,
+  getById,
+  getMovies,
+  getMoviesById,
+  addMovie,
+  deleteMovie
+} from './db';
 
 const resolvers = {
   Query: {
     people: () => people,
     person: (_, { id }) => getById(id),
-    movies: () => getMovies,
+    movies: () => getMovies(),
     movie: (_, { id }) => getMoviesById(id)
   },
   Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score)
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id)
   }
 };
 
